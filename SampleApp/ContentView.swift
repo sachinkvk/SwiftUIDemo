1f7c7631230083233eb50d3c8f78bb267c77d5a5
Sachin Panigrahi <sachinkvk14@gmail.com>
2025-11-07 14:31:41 +0530

    Initial Commit

---
+++ SampleApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SampleApp
+//
+//  Created by Sachin Panigrahi on 07/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+        
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SampleApp/SampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleAppApp.swift
+//  SampleApp
+//
+//  Created by Sachin Panigrahi on 07/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
